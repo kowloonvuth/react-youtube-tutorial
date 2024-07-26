c2792534113728745a77c64b29aa80871fb8ec8c
Rany Virakvuth <ranyvirakvuth@gmail.com>
2024-07-27 01:24:05 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
+import { Plant } from "./components/planet";
+
 function App() {
-  return (
+  const planets = [
+    { name: "Mars", isGasPlanet: false },
+    { name: "Earth", isGasPlanet: false },
+    { name: "Jupiter", isGasPlanet: true },
+    { name: "Venus", isGasPlanet: false },
+    { name: "Neptune", isGasPlanet: true },
+    { name: "Uranus", isGasPlanet: true },
+  ];
 
+  return (
     <div className="App">
-      <Job salary={900} position="Senior Developer" company="Google" experience={5}/>
-      <Job salary={200} position="Junior Developer" company="Google" experience={0}/>
-      <Job salary={300} position="UX/UI Developer" company="Google" experience={0}/>
-    </div>
-  );
-}
-
-const Job = (props) => {
-  
-  return (
-  <div>
-    <h1>{props.salary}</h1>
-    <h1>{props.position}</h1>
-    <h1>{props.company}</h1>
-    <h1>{props.experience >= 3 ? <p style={{color: "green"}}> You hired</p> : <p style={{color: "red"}}>You Failed</p>}</h1>
-  </div> 
+    {planets.map((planet, key) => planet.isGasPlanet && <Plant name={planet.name}/>)}
+  </div>
   );
 }
 
---
+++ src/components/planet.jsx
@@ -0,0 +1,8 @@
+export const Plant = (props) => {
+
+    return (
+        <div>
+            <h1>{props.name}</h1>
+        </div>
+    );
+}
